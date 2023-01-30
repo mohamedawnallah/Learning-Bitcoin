a3d190e9b0985f8aaf622a0efe8ea288ae9083df
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2023-01-30 14:52:53 +0200

    Update shasum_hashing script

--- hashing_and_mining/shasum_hashing.sh
+++ hashing_and_mining/shasum_hashing.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-shasum -a 256 data/big.txt
+file=$1
+hash_type=$2
+
+if [ -z "$hash_type" ]; then
+hash_type=256
+fi
+
+hash=$(shasum -a "$hash_type" "$file" | awk '{ print $1 }')
+
+echo "The sha$hash_type hash of the contents of $file is: $hash"
 
